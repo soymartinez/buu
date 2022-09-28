88f5d5a2f53f4cc469878649a3ecd60be1e1db8f
omgismartinez <martnzomg@gmail.com>
2022-09-27 22:01:03 -0500

    🌌 refactor (index): header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import Layout from 'components/layout'
+import Header from 'components/header'
 
 export default function Home() {
   return (
     <Layout title='SIUM'>
-      <h1 className='text-lg md:text-2xl leading-normal font-bold text-gray-700'>
-        Sistema de Información de las Universidades de México
-      </h1>
+      <Header />
     </Layout>
   )
 }
