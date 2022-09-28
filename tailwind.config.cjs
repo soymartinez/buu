9f90b4ad3c6d107325f525a8910b893d3cf78ad8
omgismartinez <martnzomg@gmail.com>
2022-09-27 21:56:43 -0500

    🚀 feat: theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#ffffff",
+      black: "#000000",
+      primary: '#2524d1',
+      secondary: '#d7dfdc',
+      font: '#999999',
+      hover: '#edefef',
+      gray: {
+        100: '#f4f4f4',
+        200: '#dadada',
+      },
+      trasparent: 'transparent',
+    },
     extend: {},
   },
   plugins: [],
