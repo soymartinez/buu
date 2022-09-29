6f4e85f2ba45efdf5f2e3f12b991796dbb54c034
omgismartinez <martnzomg@gmail.com>
2022-09-29 00:18:55 -0500

    🌌 refactor (card): image overflow, line-height title

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -11,10 +11,12 @@ export default function Card({ image, title, description, state }: CardProps) {
     return (
         <div className='flex items-center justify-between gap-4 p-3 hover:bg-hover transition-colors rounded-xl'>
             <div className='flex items-center gap-4'>
-                <div className='w-8 h-8 bg-primary rounded-md' />
-                {/* <Image src={image || ''} alt={title} width={100} height={100} /> */}
+                <div>
+                    <div className='w-8 h-8 bg-primary rounded-md' />
+                    {/* <Image src={image || ''} alt={title} width={100} height={100} /> */}
+                </div>
                 <div className='flex flex-col'>
-                    <h1 className='text-sm font-bold'>{title}</h1>
+                    <h1 className='text-sm font-bold leading-none'>{title}</h1>
                     <p className='text-[14px] text-font font-medium'>{description}</p>
                 </div>
             </div>
