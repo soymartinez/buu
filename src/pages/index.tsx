ae82bbd0cacc1e979d502edfbbc762d4508b2427
omgismartinez <martnzomg@gmail.com>
2022-09-28 23:56:27 -0500

    🌌 refactor (index): list component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Layout from 'components/layout'
 import Header from 'components/header'
+import List from 'components/list'
 
 export default function Home() {
   return (
     <Layout title='SIUM'>
       <Header />
+      <List />
     </Layout>
   )
 }
