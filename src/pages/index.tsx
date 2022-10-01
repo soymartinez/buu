32c386d4f66675bc13959d00217b959a890f3200
omgismartinez <martnzomg@gmail.com>
2022-09-30 23:07:51 -0500

    🌌 refactor (index): filter component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Layout from 'components/layout'
 import Header from 'components/header'
 import List from 'components/list'
+import Filter from 'components/filter'
 
 export default function Home() {
   return (
     <Layout title='SIUM'>
       <Header />
-      <List />
+      <section className='flex flex-col gap-4'>
+        <Filter />
+        <List />
+      </section>
     </Layout>
   )
 }
