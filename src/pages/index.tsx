3a5a0d850fe206ed94f0bebc87ced73a051aa336
omgismartinez <martnzomg@gmail.com>
2022-10-01 23:37:40 -0500

    🌌 refactor (index): filter section and list of universities

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Layout title='SIUM'>
       <Header />
-      <section className='flex flex-col gap-4'>
+      <section className='flex flex-col gap-4 px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
         <Filter />
         <List />
       </section>
