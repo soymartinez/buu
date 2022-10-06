9b42866e0fa4986194f2bf9da573325c73204da9
omgismartinez <martnzomg@gmail.com>
2022-10-05 23:08:50 -0500

    🌌 refactor (layout): favicon, browsers & platforms.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,8 +11,15 @@ export default function Layout({ title, children }: LayoutProps) {
     <div>
       <Head>
         <title>{title}</title>
-        <meta name='description' content='Sistema de Información de las Universidades de México' />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Buscador de universidades' />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='theme-color' content='#ffffff'></meta>
       </Head>
 
       <Navbar />
