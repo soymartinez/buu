df9298cf4a90f9b0912a3c257791bc9af35ec679
omgismartinez <martnzomg@gmail.com>
2022-10-11 09:59:25 -0500

    🚀 feat: universidades

---
+++ src/pages/universidades.tsx
@@ -0,0 +1,11 @@
+import Layout from 'components/layout'
+
+export default function Universidades() {
+    return (
+        <Layout title='Universidades'>
+            <div className='flex flex-col gap-4 pt-[60px] px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
+                Universidades
+            </div>
+        </Layout>
+    )
+}
