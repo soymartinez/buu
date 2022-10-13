060cd4b1ea155c753c26d6d51978afb63daaf742
omgismartinez <martnzomg@gmail.com>
2022-10-12 23:28:34 -0500

    🌌 refactor (universidades): header title

--- src/pages/universidades.tsx
+++ src/pages/universidades.tsx
@@ -2,7 +2,7 @@ import Layout from 'components/layout'
 
 export default function Universidades() {
     return (
-        <Layout title='Universidades'>
+        <Layout title='Buu – Universidades'>
             <div className='flex flex-col gap-4 pt-[60px] md:pt-[80px] px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
                 Universidades
             </div>
