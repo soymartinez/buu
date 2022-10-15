4620d645ce849308d0d66a894220ef72e7131aea
Martinez <72507996+omgismartinez@users.noreply.github.com>
2022-10-14 23:53:31 -0500

    🌌 refactor (universidades): added list component

--- src/pages/universidades.tsx
+++ src/pages/universidades.tsx
@@ -1,10 +1,13 @@
+
+import List from 'components/list'
 import Layout from 'components/layout'
 
 export default function Universidades() {
     return (
         <Layout title='Buu – Universidades'>
             <div className='flex flex-col gap-4 pt-[60px] md:pt-[80px] px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
-                Universidades
+                Universidades 
+                <List />
             </div>
         </Layout>
     )
