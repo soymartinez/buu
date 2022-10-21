ac3807ceabb285ca3d8bcecd973effe539b0a85e
omgismartinez <martnzomg@gmail.com>
2022-10-21 10:42:49 -0500

    🌌 refactor (card): props data

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,31 +1,21 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import { University } from 'pages/api/examples'
 
-export interface CardProps {
-    image?: string
-    title?: string
-    description?: string
-    state?: string
-}
-
-export default function Card({ image, title, description, state }: CardProps) {
-    const [isHovered, setIsHovered] = useState(false)
-    
+export default function Card(props: University) {
     return (
-        <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} 
+        <div
             className='flex flex-col md:flex-row md:items-center justify-between gap-4 p-3 hover:text-white md:hover:text-black 
             border md:border-none border-secondary hover:bg-primary md:hover:bg-hover transition-colors rounded-xl'>
             <div className='flex items-center gap-4'>
-                <div>
-                    <div className={`w-8 h-8 transition-colors ${isHovered ? 'bg-white' : 'bg-primary'} md:bg-primary rounded-md`} />
-                    {/* <Image src={image || ''} alt={title} width={100} height={100} /> */}
+                <div className='grid place-content-center w-10 h-10 relative'>
+                    <Image src={props.logo || ''} alt={props.name} layout={'fill'} objectFit={'contain'} />
                 </div>
                 <div className='flex flex-col'>
-                    <h1 className='text-base md:text-sm font-bold leading-none'>{title}</h1>
-                    <p className='text-[14px] text-font font-medium'>{description}</p>
+                    <h1 className='text-base md:text-sm font-bold leading-none'>{props.name}</h1>
+                    <p className='text-[14px] text-font font-medium'>{props.subname}</p>
                 </div>
             </div>
-            <h2 className='text-[16px] md:text-sm font-semibold md:font-bold'>{state}</h2>
+            <h2 className='text-[16px] md:text-sm font-semibold md:font-bold'>{props.location}</h2>
         </div>
     )
 }
