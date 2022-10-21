4e3098e0725d96c0edbc11b9a5e5c433131b25f1
omgismartinez <martnzomg@gmail.com>
2022-10-21 10:50:44 -0500

    🌌 refactor (list): get all universities from trpc

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,37 +1,16 @@
 import Link from 'next/link'
+import { University } from 'pages/api/examples'
 import { trpc } from 'utils/trpc'
-import Card, { CardProps } from './card'
+import Card from './card'
 
 export default function List() {
-
-    const list: CardProps[] = [
-        {
-            image: '',
-            title: 'Universidad Veracruzana',
-            description: 'UV',
-            state: 'Veracruz'
-        },
-        {
-            image: '',
-            title: 'Universidad Nacional Autónoma de México',
-            description: 'UNAM',
-            state: 'Ciudad de México'
-        },
-        {
-            image: '',
-            title: 'Universidad Autónoma de Nuevo León',
-            description: 'UANL',
-            state: 'Nuevo León'
-        },
-    ]
-
-    const {data} = trpc.useQuery(['example.hello'])
-    console.log(data)
+    const { data, isLoading } = trpc.useQuery(['university.getAll'])
+    if (!data || isLoading) return <div>Loading...</div>
 
     return (
         <div className='flex flex-col gap-2'>
-            {list.map((item, index) => (
-                <Link href={`/universidades/${item.description?.toLocaleLowerCase()}`} key={index}>
+            {data.map((item: University, index: number) => (
+                <Link href={`/universidades/${item.subname?.toLocaleLowerCase()}`} key={index}>
                     <a>
                         <Card {...item} />
                     </a>
