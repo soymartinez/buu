34957e41f7848ee5e2555fbe20a1df788f6a6be0
omgismartinez <martnzomg@gmail.com>
2022-10-26 22:21:28 -0500

    🌌 refactor (id): trpc getOne

--- src/pages/universidades/[id].tsx
+++ src/pages/universidades/[id].tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from 'next'
 import Layout from 'components/layout'
+import { trpc } from 'utils/trpc'
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     return {
@@ -8,6 +9,13 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 }
 
 export default function Universidad({ id }: { id: string }) {
+    const { data, isLoading, isError } = trpc.useQuery(['university.getOne', {
+        text: id
+    }])
+
+    if (!data || isLoading) return <div>loading...</div>
+    if (isError) return <div>error</div>
+    
     return (
         <Layout title={`Buu – ${id?.toLocaleString().toUpperCase()}`}>
             <div className='flex flex-col gap-4 pt-[70px] md:pt-[80px] px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
