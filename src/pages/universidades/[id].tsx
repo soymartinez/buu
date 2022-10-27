732675cb0ee7fa999df3827ab22dbb3a50a2132c
omgismartinez <martnzomg@gmail.com>
2022-10-27 17:11:56 -0500

    🌌 refactor (id): get unique university by subname

--- src/pages/universidades/[id].tsx
+++ src/pages/universidades/[id].tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from 'next'
 import Layout from 'components/layout'
 import { trpc } from 'utils/trpc'
+import Image from 'next/image'
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
     return {
@@ -9,17 +10,44 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 }
 
 export default function Universidad({ id }: { id: string }) {
-    const { data, isLoading, isError } = trpc.useQuery(['university.getOne', {
+    const { data, isLoading, isError } = trpc.useQuery(['university.getOneBySubname', {
         text: id
     }])
 
     if (!data || isLoading) return <div>loading...</div>
     if (isError) return <div>error</div>
-    
+
     return (
         <Layout title={`Buu – ${id?.toLocaleString().toUpperCase()}`}>
             <div className='flex flex-col gap-4 pt-[70px] md:pt-[80px] px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
-                <h1 className='font-bold text-xl md:text-2xl mt-4 uppercase'>{id}</h1>
+                <h1 className='font-bold text-xl md:text-2xl mt-4'>{data.name}</h1>
+                <p className='text-sm md:text-base'>{data.subname}</p>
+                <Image src={data.logo} width={200} height={200} objectFit={'contain'} />
+                <div>
+                    <a href={data.url || ''} className='text-sm md:text-base'>{data.url}</a>
+                    <p className='text-sm md:text-base'>{data.description}</p>
+                    <p className='text-sm md:text-base'>{data.type}</p>
+                    <p className='text-sm md:text-base'>{data.ranking}</p>
+                    <p className='text-sm md:text-base'>{data.location}</p>
+                    <p className='text-sm md:text-base'>{data.country}</p>
+                </div>
+                <div>
+                    <p className='text-sm md:text-base font-bold'>Regiones</p>
+                    <ul>
+                        {data.regions.map((region) => (
+                            <div>
+                                <li key={region.id}>{region.name}</li>
+                                <ul>
+                                    {region.campus.map((campus) => {
+                                        return (
+                                            <li className='list-disc list-inside' key={campus.id}>{campus.name}</li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </Layout>
     )
