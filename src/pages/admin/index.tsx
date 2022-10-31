b93f073952504ee488f5966bab9c2a1ee2fbdfef
omgismartinez <martnzomg@gmail.com>
2022-10-30 21:28:42 -0600

    🚀 feat: admin page

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,11 @@
+import Layout from 'components/layout'
+
+export default function Admin() {
+    return (
+        <Layout title={'Buu – Administrador'}>
+            <div className='flex flex-col gap-4 pt-[70px] md:pt-[80px] px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
+                <h1 className='font-bold text-xl md:text-2xl mt-4'>Administrador</h1>
+            </div>
+        </Layout>
+    )
+}
