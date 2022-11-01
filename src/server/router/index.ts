db2bdaaa95bd2ac86f7e23f8743ac17318df1572
omgismartinez <martnzomg@gmail.com>
2022-10-31 20:30:26 -0600

    🌌 refactor (router): regionRouter

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -5,11 +5,13 @@ import superjson from "superjson";
 import { universityRouter } from './universityRouter'
 import { protectedExampleRouter } from "./protected-example-router";
 import { carrerRouter } from "./carrerRouter";
+import { regionRouter } from "./regionRouter";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("university.", universityRouter)
   .merge("carrer.", carrerRouter)
+  .merge("region.", regionRouter)
   .merge("auth.", protectedExampleRouter);
 
 // export type definition of API
