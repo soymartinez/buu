c0e135dd8830e491e4fb07ae57ad111205b7204f
omgismartinez <martnzomg@gmail.com>
2022-11-02 23:10:39 -0600

    🌌 refactor (universityRouter): create

--- src/server/router/universityRouter.ts
+++ src/server/router/universityRouter.ts
@@ -1,5 +1,6 @@
 import { createRouter } from "./context";
 import { z } from "zod";
+import { Type } from "@prisma/client";
 
 export const universityRouter = createRouter()
   .query("getOneBySubname", {
@@ -31,4 +32,30 @@ export const universityRouter = createRouter()
     async resolve({ ctx }) {
       return await ctx.prisma.university.findMany()
     },
-  });
+  })
+  .mutation('create', {
+    input: z.object({
+      name: z.string(),
+      subname: z.string(),
+      logo: z.string(),
+      url: z.string(),
+      description: z.string(),
+      type: z.string(),
+      ranking: z.number().optional(),
+      location: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.university.create({
+        data: {
+          name: input.name,
+          subname: input.subname,
+          logo: input.logo,
+          url: input.url,
+          description: input.description,
+          type: input.type as Type,
+          ranking: input.ranking,
+          location: input.location,
+        },
+      })
+    }
+  })
