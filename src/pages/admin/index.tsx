d3a1ebff6d9f9cb81a3cb76f2884666fba4d4555
omgismartinez <martnzomg@gmail.com>
2022-11-03 21:08:33 -0600

    🌌 refactor (admin): add new region and modified university fields

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -4,8 +4,9 @@ import { FormEvent, useState } from 'react'
 import { trpc } from 'utils/trpc'
 
 export default function Admin() {
-    const { data: region } = trpc.useQuery(['region.getAll'])
-    const { mutate } = trpc.useMutation(['university.create'])
+    const { data: regions } = trpc.useQuery(['region.getAll'])
+    const { mutate: universityCreate } = trpc.useMutation(['university.create'])
+    const { mutate: regionCreate } = trpc.useMutation(['region.create'])
 
     const [status, setStatus] = useState('universidad')
 
@@ -13,20 +14,35 @@ export default function Admin() {
         e.preventDefault()
         const formData = new FormData(e.target as HTMLFormElement)
         const data = Object.fromEntries(formData.entries())
-        mutate({
-            name: data.name as string,
-            subname: data.subname as string,
-            logo: data.logo as string,
-            url: data.url as string,
-            description: data.description as string,
-            location: data.location as string,
-            ranking: Number(data.ranking),
-            type: data.type as Type,
-        }, {
-            onSuccess() {
-                setStatus('region')
-            },
-        })
+        // universityCreate({
+        //     name: data.name as string,
+        //     subname: data.subname as string,
+        //     logo: data.logo as string,
+        //     url: data.url as string,
+        //     description: data.description as string,
+        //     location: data.location as string,
+        //     ranking: Number(data.ranking),
+        //     type: data.type as Type,
+        // }, {
+        //     onSuccess() {
+        //         setStatus('region')
+        //     },
+        // })
+        setStatus('region')
+    }
+
+    const handleSaveRegion = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const formData = new FormData(e.target as HTMLFormElement)
+        const data = Object.fromEntries(formData.entries())
+        // regionCreate({
+        //     name: data.name as string,
+        // }, {
+        //     onSuccess() {
+        //         setStatus('carrer')
+        //     },
+        // })
+        setStatus('carrera')
     }
 
     return (
@@ -37,27 +53,31 @@ export default function Admin() {
                     <h2 className='font-bold text-lg md:text-xl text-end'>Nueva {status}</h2>
                     {status === 'universidad' && (
                         <form onSubmit={(e) => handleSaveUniversity(e)} className='flex flex-col gap-1'>
-                            <div className='grid grid-cols-2 gap-1'>
-                                <span className='text-sm font-bold'>Nombre</span>
-                                <span className='text-sm font-bold'>Subnombre</span>
-                                <input type={'text'} required name={'name'} className='border border-gray-300 rounded-md px-2 py-1' />
-                                <input type={'text'} required name={'subname'} className='border border-gray-300 rounded-md px-2 py-1' />
+                            <div className='grid md:grid-cols-2 gap-2'>
+                                <div className='flex flex-col gap-1'>
+                                    <span className='text-sm font-bold'>Nombre</span>
+                                    <input type={'text'} required name={'name'} className='border border-gray-300 rounded-md px-2 py-1' />
+                                </div>
+                                <div className='flex flex-col gap-1'>
+                                    <span className='text-sm font-bold'>Subnombre <span className='text-xs text-font'>(opcional)</span></span>
+                                    <input type={'text'} name={'subname'} className='border border-gray-300 rounded-md px-2 py-1' />
+                                </div>
                             </div>
 
                             <span className='text-sm font-bold'>Logo</span>
                             <input type={'text'} required name={'logo'} className='border border-gray-300 rounded-md px-2 py-1' />
 
-                            <span className='text-sm font-bold'>URL</span>
+                            <span className='text-sm font-bold'>URL <span className='text-xs text-font'>(opcional)</span></span>
                             <input type={'url'} name={'url'} className='border border-gray-300 rounded-md px-2 py-1' />
 
-                            <span className='text-sm font-bold'>Descripción</span>
+                            <span className='text-sm font-bold'>Descripción <span className='text-xs text-font'>(opcional)</span></span>
                             <textarea rows={3} name={'description'} className='border border-gray-300 rounded-md px-2 py-1' />
 
                             <span className='text-sm font-bold'>Localidad</span>
                             <input type={'text'} required name={'location'} className='border border-gray-300 rounded-md px-2 py-1' />
 
-                            <span className='text-sm font-bold'>Ranking</span>
-                            <input type={'number'} name={'ranking'} min={1} max={100} className='border border-gray-300 rounded-md px-2 py-1' />
+                            <span className='text-sm font-bold'>Ranking <span className='text-xs text-font'>(opcional)</span></span>
+                            <input type={'number'} name={'ranking'} defaultValue={1} min={1} max={100} className='border border-gray-300 rounded-md px-2 py-1' />
 
                             <span className='text-sm font-bold'>Tipo</span>
                             <select required name={'type'} className='border border-gray-300 rounded-md px-2 py-1'>
@@ -65,17 +85,31 @@ export default function Admin() {
                                 <option value={Type.Privada}>Privada</option>
                             </select>
 
-                            <span className='text-sm font-bold'>Region</span>
-                            <select required name={'region'} className='border border-gray-300 rounded-md px-2 py-1'>
-                                {region && region.map(({ id, name }) => (
-                                    <option key={id}>
-                                        {name}
-                                    </option>
-                                ))}
-                            </select>
-
-                            <button className='bg-primary text-white font-bold rounded-full w-min px-4 self-center' type={'submit'}>Guardar</button>
+                            <button className='bg-primary text-white font-bold rounded-full w-min px-4 mt-2' type={'submit'}>Guardar</button>
                         </form>
+                    )}
+                    {status === 'region' && (
+                        <>
+                            <form onSubmit={(e) => handleSaveRegion(e)} className='flex flex-col gap-1'>
+                                <span className='text-sm font-bold'>Nombre</span>
+                                <input type={'text'} required name={'name'} className='border border-gray-300 rounded-md px-2 py-1' />
+
+                                <div className='flex gap-2 mt-2'>
+                                    <button className='bg-primary text-white font-bold rounded-full w-min px-4' onClick={() => setStatus('universidad')} type={'button'}>Regresar</button>
+                                    <button className='bg-primary text-white font-bold rounded-full w-min px-4' type={'submit'}>Guardar</button>
+                                </div>
+                            </form>
+                            <div>
+                                <h3 className='font-bold text-lg md:text-xl text-end'>Regiones</h3>
+                                <div className='flex flex-col gap-2'>
+                                    {regions?.map((region) => (
+                                        <div key={region.id} className='flex flex-col gap-1'>
+                                            <span className='text-sm font-bold'>{region.name}</span>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </>
                     )}
                 </div>
             </div>
