492cbb87ac19c497f6e02503586e1687a2d1099c
omgismartinez <martnzomg@gmail.com>
2022-11-03 21:09:40 -0600

    🌌 refactor (regionRouter): create

--- src/server/router/regionRouter.ts
+++ src/server/router/regionRouter.ts
@@ -1,8 +1,21 @@
 import { createRouter } from './context'
+import { z } from 'zod'
 
 export const regionRouter = createRouter()
     .query('getAll', {
         async resolve({ ctx }) {
             return await ctx.prisma.region.findMany()
+        }
+    })
+    .mutation('create', {
+        input: z.object({
+            name: z.string()
+        }),
+        async resolve({ ctx, input }) {
+            return await ctx.prisma.region.create({
+                data: {
+                    name: input.name
+                }
+            })
         }
     })
