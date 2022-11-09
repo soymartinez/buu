3b4b9a51c0e351f9ee283e15dac5009891094717
omgismartinez <martnzomg@gmail.com>
2022-11-08 22:31:40 -0600

    🌌 refactor (router): campusRouter

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -6,12 +6,14 @@ import { universityRouter } from './universityRouter'
 import { protectedExampleRouter } from "./protected-example-router";
 import { carrerRouter } from "./carrerRouter";
 import { regionRouter } from "./regionRouter";
+import { campusRouter } from "./campusRouter";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("university.", universityRouter)
   .merge("carrer.", carrerRouter)
   .merge("region.", regionRouter)
+  .merge("campus.", campusRouter)
   .merge("auth.", protectedExampleRouter);
 
 // export type definition of API
