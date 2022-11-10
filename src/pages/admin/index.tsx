b71865846e2a628856a0ec562eca50469f1c346d
omgismartinez <martnzomg@gmail.com>
2022-11-09 18:39:53 -0600

    🌌 refactor (admin): modal state and active menu item

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -15,8 +15,8 @@ export default function Admin() {
     const { mutate: universityCreate } = trpc.useMutation(['university.create'])
     const { mutate: regionCreate } = trpc.useMutation(['region.create'])
 
-    const [status, setStatus] = useState('universidades')
-    const [addUniversity, setAddUniversity] = useState(false)
+    const [status, setStatus] = useState('universidad')
+    const [modal, setModal] = useState(false)
 
     const handleSaveUniversity = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -54,25 +54,25 @@ export default function Admin() {
     }
 
     const menu = [
-        { name: 'universidades', count: universities?.length },
-        { name: 'regiones', count: regions?.length },
-        { name: 'campus', count: campus?.length },
-        { name: 'carreras', count: carrers?.length },
+        { name: 'universidades', active: 'universidad', count: universities?.length },
+        { name: 'regiones', active: 'región', count: regions?.length },
+        { name: 'campus', active: 'campus', count: campus?.length },
+        { name: 'carreras', active: 'carrera', count: carrers?.length },
     ]
 
     return (
         <Layout title={'Buu – Administrador'}>
             <div className='flex flex-col gap-4 pt-[70px] md:pt-[80px] px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
                 <h1 className='font-bold text-xl md:text-2xl mt-4'>Administrador</h1>
-                <section className='grid gap-2'>
+                <section className='grid gap-4'>
                     <div className='flex items-center gap-1 sm:gap-6 bg-[#ececec] rounded-xl px-2 overflow-x-auto'>
-                        {menu.map(({ name, count }) => (
-                            <div key={name} >
+                        {menu.map(({ name, active, count }) => (
+                            <div key={name} onClick={() => { setStatus(active); setModal(false) }}>
                                 <div className={`flex md:flex-row items-center justify-between font-bold gap-2 py-2 px-1 md:px-3 transition-colors cursor-pointer 
-                                    ${status === name ? 'text-primary' : 'text-font hover:text-font/70'}`} onClick={() => setStatus(name)}>
+                                    ${status === active ? 'text-primary' : 'text-font hover:text-font/70'}`}>
                                     <h2 className='text-xs capitalize'>{name}</h2>
                                     {count ?
-                                        <div className={`grid place-content-center rounded-full w-6 h-6 ${status === name ? 'bg-primary/20' : 'bg-[#d9d9d9]'}`}>
+                                        <div className={`grid place-content-center rounded-full w-6 h-6 ${status === active ? 'bg-primary/20' : 'bg-[#d9d9d9]'}`}>
                                             <h1 className='text-[13px]'>{count}</h1>
                                         </div>
                                         : <Skeleton circle={true} baseColor={'#d9d9d9'} width={30} containerClassName={'skeleton-container'} inline={true} height={30} />}
@@ -81,13 +81,32 @@ export default function Admin() {
                             </div>
                         ))}
                     </div>
-                    <div onClick={() => setAddUniversity(!addUniversity)}
-                        className={`flex gap-3 px-3 md:px-5 py-2 w-min font-semibold text-font hover:opacity-70 cursor-pointer ml-auto`}>
-                        <div className='flex justify-center items-center relative'>
-                            <div className='w-[12px] h-[2px] rounded-full bg-font' />
-                            <div className='absolute w-[12px] h-[2px] rounded-full bg-font rotate-90' />
+                    <div className='flex flex-col gap-4 lg:flex-row lg:items-center justify-between'>
+                        <div onClick={() => setModal(!modal)}
+                            className={`flex gap-3 px-3 md:px-5 py-2 w-min font-semibold text-font hover:opacity-70 cursor-pointer ml-auto lg:m-0`}>
+                            <div className='flex justify-center items-center relative'>
+                                <div className='w-[12px] h-[2px] rounded-full bg-font' />
+                                <div className='absolute w-[12px] h-[2px] rounded-full bg-font rotate-90' />
+                            </div>
+                            <h2 className='text-xs select-none whitespace-nowrap'>Agregar <span className='lowercase'>{status}</span></h2>
                         </div>
-                        <h2 className='text-xs select-none whitespace-nowrap'>Agregar <span className='lowercase'>universidad</span></h2>
+                        <div className='flex flex-row items-center gap-2'>
+                            <div className='flex items-center w-full'>
+                                <label className='sr-only'>Buscar</label>
+                                <div className='relative w-full'>
+                                    <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+                                        <svg className='w-5 h-5 text-font' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z' clip-rule='evenodd'></path></svg>
+                                    </div>
+                                    <input type='text' id='table-search' className='placeholder:text-font block p-2 pl-10 w-full lg:w-80 text-xs rounded-lg border' placeholder='Buscar por nombre' />
+                                </div>
+                            </div>
+                            <div className='flex items-center gap-2'>
+                                <select className='border rounded-lg text-xs text-font px-2 py-2'>
+                                    <option className='py-2' value={''}>Ascendente</option>
+                                    <option className='py-2' value={''}>Descendente</option>
+                                </select>
+                            </div>
+                        </div>
                     </div>
                 </section>
             </div>
