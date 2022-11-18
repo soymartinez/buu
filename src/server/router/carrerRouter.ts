a1106a8fc45b27f82623d49b59d9f07b28546997
omgismartinez <martnzomg@gmail.com>
2022-11-17 22:35:44 -0600

    🌌 refactor (careerRouter): create

--- src/server/router/carrerRouter.ts
+++ src/server/router/carrerRouter.ts
@@ -1,7 +1,9 @@
 import { createRouter } from './context'
+import { z } from 'zod'
+import { Modality } from '@prisma/client'
 
 export const carrerRouter = createRouter()
-    .query("getAll", {
+    .query('getAll', {
         async resolve({ ctx }) {
             return await ctx.prisma.carrer.findMany({
                 include: {
@@ -10,4 +12,34 @@ export const carrerRouter = createRouter()
                 }
             })
         },
-    });
+    })
+    .mutation('create', {
+        input: z.object({
+            name: z.string(),
+            modality: z.string(),
+            semesters: z.number(),
+            curriculum: z.string(),
+            universityId: z.number(),
+            campusId: z.number(),
+        }),
+        async resolve({ ctx, input }) {
+            return await ctx.prisma.carrer.create({
+                data: {
+                    name: input.name,
+                    modality: input.modality as Modality,
+                    semesters: input.semesters,
+                    curriculum: input.curriculum,
+                    university: {
+                        connect: {
+                            id: input.universityId
+                        }
+                    },
+                    campus: {
+                        connect: {
+                            id: input.campusId
+                        }
+                    },
+                }
+            })
+        }
+    })
