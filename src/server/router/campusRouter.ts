72f1a4076db3e93d021889d1bf9594792f01a824
omgismartinez <martnzomg@gmail.com>
2022-11-20 21:11:16 -0600

    🌌 refactor (campusRouter): mutation create new campus

--- src/server/router/campusRouter.ts
+++ src/server/router/campusRouter.ts
@@ -1,12 +1,41 @@
 import { createRouter } from './context'
+import { z } from 'zod'
 
 export const campusRouter = createRouter()
-    .query("getAll", {
+    .query('getAll', {
         async resolve({ ctx }) {
             return await ctx.prisma.campus.findMany({
                 include: {
                     region: true,
                     careers: true,
+                }
+            })
+        },
+    })
+    .mutation('create', {
+        input: z.object({
+            name: z.string(),
+            subname: z.string(),
+            direction: z.string(),
+            contact: z.string(),
+            url: z.string(),
+            location: z.string(),
+            region: z.number(),
+        }),
+        async resolve({ ctx, input }) {
+            return await ctx.prisma.campus.create({
+                data: {
+                    name: input.name,
+                    subname: input.subname,
+                    direction: input.direction,
+                    contact: input.contact,
+                    url: input.url,
+                    location: input.location,
+                    region: {
+                        connect: {
+                            id: input.region
+                        }
+                    },
                 }
             })
         },
