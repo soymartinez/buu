d36f29a5072e22e3ef23897a68bcee23f8879b7d
omgismartinez <martnzomg@gmail.com>
2022-11-21 01:32:31 -0600

    🌌 refactor (careerRouter): rename methods and logic

--- src/server/router/careerRouter.ts
+++ src/server/router/careerRouter.ts
@@ -1,44 +1,84 @@
 import { createRouter } from './context'
 import { z } from 'zod'
-import { Modality } from '@prisma/client'
+import { Level, Modality, Period } from '@prisma/client'
 
 export const careerRouter = createRouter()
-    .query('getAll', {
+    .query('getAllCareers', {
+        async resolve({ ctx }) {
+            return await ctx.prisma.careers.findMany({
+                include: {
+                    careers: {
+                        include: {
+                            campus: true,
+                            university: true,
+                        }
+                    }
+                }
+            })
+        },
+    })
+    .query('getAllCareersDetails', {
         async resolve({ ctx }) {
             return await ctx.prisma.career.findMany({
                 include: {
-                    campus: true,
+                    career: true,
                     university: true,
+                    campus: true,
+                }
+            })
+        }
+    })
+    .query('getOne', {
+        input: z.object({
+            name: z.string(),
+        }),
+        async resolve({ ctx, input }) {
+            return await ctx.prisma.careers.findUnique({
+                where: {
+                    name: input.name,
+                },
+                include: {
+                    careers: true
                 }
             })
         },
     })
     .mutation('create', {
         input: z.object({
-            name: z.string(),
+            career: z.number(),
+            university: z.number(),
+            campus: z.number(),
+            level: z.string(),
+            area: z.string(),
+            period: z.string(),
+            duration: z.number(),
+            program: z.string(),
             modality: z.string(),
-            semesters: z.number(),
-            curriculum: z.string(),
-            universityId: z.number(),
-            campusId: z.number(),
         }),
         async resolve({ ctx, input }) {
             return await ctx.prisma.career.create({
                 data: {
-                    name: input.name,
-                    modality: input.modality as Modality,
-                    semesters: input.semesters,
-                    curriculum: input.curriculum,
+                    career: {
+                        connect: {
+                            id: input.career,
+                        }
+                    },
                     university: {
                         connect: {
-                            id: input.universityId
+                            id: input.university,
                         }
                     },
                     campus: {
                         connect: {
-                            id: input.campusId
+                            id: input.campus
                         }
                     },
+                    level: input.level as Level,
+                    area: input.area,
+                    period: input.period as Period,
+                    duration: input.duration,
+                    program: input.program,
+                    modality: input.modality as Modality,
                 }
             })
         }
