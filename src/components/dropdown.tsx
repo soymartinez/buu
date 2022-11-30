f9799216606b8c91f5c5c99eb81a851b0176e0e9
omgismartinez <martnzomg@gmail.com>
2022-11-30 00:25:11 -0600

    🌌 refactor (dropdown): z-index

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -10,7 +10,7 @@ export default function Dropdown({ title, object, setStatus }: { title: string, 
                 <svg className={`ml-auto w-4 h-4 text-black transition-all -rotate-90 ${isOpen && 'rotate-0'}`} fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z' clipRule='evenodd'></path></svg>
             </button>
 
-            <div className={`${!isOpen ? 'hidden' : 'focus:border-black'} absolute bg-white z-50 rounded-b-md border border-hover shadow-lg`}>
+            <div className={`${!isOpen ? 'hidden' : 'focus:border-black'} absolute bg-white z-20 rounded-b-md border border-hover shadow-lg`}>
                 <div className='flex items-center w-full p-3'>
                     <label className='sr-only'>Buscar</label>
                     <div className='relative w-full'>
