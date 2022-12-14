4d65f53b28c0bb21fbab24bf7623eb261b0f73a6
omgismartinez <martnzomg@gmail.com>
2022-12-14 16:29:07 -0600

    🌌 refactor (list): skeleton replicas

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -14,18 +14,21 @@ export default function List() {
                         <Card {...item} />
                     </a>
                 </Link>
-            )) : (
-                <div className='flex flex-col md:flex-row md:items-center justify-between gap-4 p-3 
+            )) :
+                [0, 1, 2, 3, 4].map((_, index) => (
+                    <div key={index} className='flex flex-col md:flex-row md:items-center justify-between gap-4 p-3 
                     border md:border-none border-secondary transition-colors rounded-xl'>
-                    <div className='flex items-center gap-4'>
-                        <Skeleton containerClassName={'skeleton-container'} height={50} width={50} inline={true} />
-                        <div className='flex flex-col'>
-                            <h1 className='text-base md:text-sm font-bold leading-none'>{<Skeleton width={200} />}</h1>
-                            <p className='text-[14px] text-font font-medium'>{<Skeleton width={50} />}</p>
+                        <div className='flex items-center gap-4'>
+                            <Skeleton containerClassName={'skeleton-container'} height={50} width={50} inline={true} />
+                            <div className='flex flex-col'>
+                                <h1 className='text-base md:text-sm font-bold leading-none'>{<Skeleton width={200} />}</h1>
+                                <p className='text-[14px] text-font font-medium'>{<Skeleton width={50} />}</p>
+                            </div>
                         </div>
+                        <h2 className='text-[16px] md:text-sm font-semibold md:font-bold'>{<Skeleton width={100} />}</h2>
                     </div>
-                    <h2 className='text-[16px] md:text-sm font-semibold md:font-bold'>{<Skeleton width={100} />}</h2>
-                </div>)}
+                ))
+            }
         </div>
     )
 }
