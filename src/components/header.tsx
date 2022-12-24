9631a7f0871dd3e850ff07fe36e4be49fd9a15e1
omgismartinez <martnzomg@gmail.com>
2022-12-23 23:48:23 -0600

    🌌 refactor (header): search event

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
-export default function Header() {
+export default function Header({
+    setSearch
+}: {
+    setSearch: (search: string) => void
+}) {
     const [peep, setPeep] = useState(0)
 
     const timer = (ms: number) => new Promise(res => setTimeout(res, ms))
@@ -24,7 +28,7 @@ export default function Header() {
                 <div className='flex justify-between z-20 gap-2 md:pl-6 pl-4 pr-2 py-2 bg-gradient-to-r from-white to-[#ffffffcc] backdrop-blur-sm rounded-full text-sm w-full'>
                     <div className='flex items-center w-full gap-3 md:gap-4'>
                         <div><svg className='w-5 lg:w-6 text-font' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z' clipRule='evenodd'></path></svg></div>
-                        <input type='text' placeholder='Universidad' className='bg-trasparent truncate focus:outline-none w-full md:h-10 md:w-2/4' />
+                        <input onChange={(e) => setSearch(e.target.value)} type='text' placeholder='Universidad' className='bg-trasparent truncate focus:outline-none w-full md:h-10 md:w-2/4' />
                         <div className='w-0.5 h-10 bg-gray-200 hidden lg:block' />
                         <input type='text' placeholder='Añadir país o ciudad' className='bg-trasparent truncate focus:outline-none w-full h-full hidden lg:block' />
                     </div>
