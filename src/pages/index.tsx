30658a34c2923f98e90515b4c100b954a09c8c36
omgismartinez <martnzomg@gmail.com>
2022-12-24 00:03:10 -0600

    🌌 refactor (index): search header to list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,17 @@ import Header from 'components/header'
 import List from 'components/list'
 import Chips from 'components/chips'
 import Filter from 'components/filter'
+import { useState } from 'react'
 
 export default function Home() {
+  const [search, setSearch] = useState('')
   return (
     <Layout title='Buu – Encuentra la mejor universidad para ti'>
-      <Header />
+      <Header setSearch={setSearch} />
       <Chips />
       <section className='flex gap-4 px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
         <Filter />
-        <List />
+        <List search={search} />
       </section>
     </Layout>
   )
