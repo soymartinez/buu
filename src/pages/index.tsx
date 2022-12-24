79cca41d357803ac777e00c3469334a7a5ed95ac
omgismartinez <martnzomg@gmail.com>
2022-12-23 21:47:29 -0600

    🌌 refactor (index): chips component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Layout from 'components/layout'
 import Header from 'components/header'
 import List from 'components/list'
+import Chips from 'components/chips'
 import Filter from 'components/filter'
 
 export default function Home() {
   return (
     <Layout title='Buu – Encuentra la mejor universidad para ti'>
       <Header />
+      <Chips />
       <section className='flex gap-4 px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
         <Filter />
         <List />
