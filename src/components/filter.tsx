519325947938a81749e2d55a0dc02d37bbe6f0be
omgismartinez <martnzomg@gmail.com>
2022-12-26 01:34:35 -0600

    🌌 refactor (filter): arrow icon component

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import Arrow from './icons/arrow'
 
 export interface Filter {
     public: boolean,
@@ -15,22 +16,6 @@ export default function Filter({ setFilter }: { setFilter: (filter: Filter) => v
     useEffect(() => {
         setFilter(filter)
     }, [filter])
-
-    const Arrow = ({ className }: {
-        className: string
-    }) => (
-        <svg
-            width={12}
-            height={12}
-            viewBox='0 0 24 24'
-            fill='currentColor'
-            className={className}
-        >
-            <path
-                d="m16.23 23.58 2.24-2a.5.5 0 0 0 0-.71L10.57 12l7.93-8.87a.5.5 0 0 0 0-.71l-2.24-2a.5.5 0 0 0-.71 0L5.2 12l10.32 11.54a.5.5 0 0 0 .71.04Z"
-            />
-        </svg>
-    )
 
     return (
         <aside className='hidden lg:flex'>
