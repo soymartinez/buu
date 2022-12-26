29011afbfd9387bfb936a3551aa94723247365b7
omgismartinez <martnzomg@gmail.com>
2022-12-26 16:31:07 -0600

    🌌 refactor (index): chips to list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,15 @@ import { useState } from 'react'
 
 export default function Home() {
   const [search, setSearch] = useState('')
+  const [chips, setChips] = useState([])
   const [filter, setFilter] = useState({})
   return (
     <Layout title='Buu – Encuentra la mejor universidad para ti'>
       <Header setSearch={setSearch} />
-      <Chips />
+      <Chips setChips={setChips} />
       <section className='flex gap-4 px-4 md:px-8 w-full max-w-[1280px] mx-auto'>
         <Filter setFilter={setFilter} />
-        <List search={search} filter={filter} />
+        <List search={search} chips={chips} filter={filter} />
       </section>
     </Layout>
   )
