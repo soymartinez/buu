07fef074deee3e7ae839328c6997246e2eacc2dc
omgismartinez <martnzomg@gmail.com>
2023-01-02 23:31:04 -0600

    🌌 refactor (select): font size

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -5,13 +5,13 @@ export default function Select({ object, name, defaultValue }: { object: object,
     const [selected, setSelected] = useState(defaultValue || Object.values(object)[0])
     return (
         <div className='relative'>
-            <button type={'button'} onClick={() => setIsOpen(!isOpen)} className='inline-flex w-full bg-hover py-3 px-4' >
+            <button type={'button'} onClick={() => setIsOpen(!isOpen)} className='inline-flex w-full bg-hover p-2' >
                 {selected}
             </button>
-            <div className={`${!isOpen ? 'hidden' : ''} absolute bg-white z-20 rounded-b-md border border-hover shadow-lg w-full`}>
-                <ul className='overflow-y-auto p-1 text-sm'>
+            <div className={`${!isOpen ? 'hidden' : ''} absolute bg-white z-20 rounded-b-xl border border-hover shadow-lg w-full`}>
+                <ul className='overflow-y-auto p-1 space-y-[2px]'>
                     {Object.values(object).map((value, index) => (
-                        <li key={index} className='flex items-center justify-between text-black hover:bg-hover rounded-lg'>
+                        <li key={index} className={`flex items-center justify-between text-black rounded-lg ${selected === value ? 'bg-hover' : 'hover:bg-hover'}`}>
                             <div className='flex items-center w-full'>
                                 <input
                                     hidden
@@ -22,7 +22,7 @@ export default function Select({ object, name, defaultValue }: { object: object,
                                     value={value}
                                     name={name}
                                     className='w-3 h-3 cursor-pointer accent-primary' />
-                                <label htmlFor={value} className={`text-xs cursor-pointer p-2 w-full`}>{value}</label>
+                                <label htmlFor={value} className={`cursor-pointer p-2 w-full`}>{value}</label>
                             </div>
                         </li>
                     ))}
