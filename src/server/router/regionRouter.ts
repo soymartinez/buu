785975bf041bb4343f95873385c2b3ccee3eebbe
omgismartinez <martnzomg@gmail.com>
2023-01-03 18:04:12 -0600

    🚀 feat (regionRouter): upsert

--- src/server/router/regionRouter.ts
+++ src/server/router/regionRouter.ts
@@ -14,11 +14,18 @@ export const regionRouter = createRouter()
     })
     .mutation('create', {
         input: z.object({
+            id: z.number().optional(),
             name: z.string()
         }),
         async resolve({ ctx, input }) {
-            return await ctx.prisma.region.create({
-                data: {
+            return await ctx.prisma.region.upsert({
+                where: {
+                    id: input.id === undefined ? -1 : input.id,
+                },
+                update: {
+                    name: input.name
+                },
+                create: {
                     name: input.name
                 }
             })
